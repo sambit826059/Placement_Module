e8c5f42ec295cb3bedfe5a55c5ea6a561c53f4ae
soumen bhunia <sob99338@gmail.com>
2023-09-20 00:48:17 +0530

    home component added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+// import NavBar from "./NavBar";
+import HeadBanner from "./HeadBanner";
+import GoToTop from './GoToTop';
+
+
+export default function Home() {
+  return (
+    <>
+      <HeadBanner/>
+
+      <GoToTop/>
+    </>
+  )
+}
