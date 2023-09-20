62a5b5da566746df5eca8a398c0a4f1feda6c40f
soumen bhunia <sob99338@gmail.com>
2023-09-20 23:50:44 +0530

    slider added on home

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 // import NavBar from "./NavBar";
 import HeadBanner from "./HeadBanner";
 import GoToTop from './GoToTop';
-
+import SliderHeadBanner from "./SliderHeadBanner";
 
 export default function Home() {
   return (
     <>
-      <HeadBanner/>
+      <SliderHeadBanner/>
 
       <GoToTop/>
     </>
