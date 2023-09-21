9e64627cf6c9086d93568740c7389d496b91c5c8
soumen bhunia <sob99338@gmail.com>
2023-09-22 01:40:18 +0530

    nav Links responsiveness updated for the mobile screen

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -23,10 +23,10 @@ function Navbar() {
                     <div className="sm:hidden  grid justify-end  ">
                     <img className=' p-2  hover:bg-gray-300  rounded' src={OpenMenu} alt="menu-open" />
                 </div>
-                <div className="max-sm:absolute  min-md:relative  ">
+                <div className=" left-0 top-[3.5rem]  max-sm:absolute  max-sm:bg-green-200  max-sm:w-[100%]   min-md:relative  min-md:bg-green-00">
                     <ul className=' grid  gap-2  sm:flex  sm:gap-4 sm:justify-end '>
 
-                    <Link  to ="/"> <li className='hover:bg-gray-300 px-2 py-1 rounded text-center'>Home</li></Link> 
+                    <Link  to ="/"> <li className='hover:bg-gray-300 px-2 py-1 rounded md:text-center'>Home</li></Link> 
 
 
                         <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group'>
@@ -34,7 +34,7 @@ function Navbar() {
                          <DropBoxLink LinkName={"Resource"}  Option_1={"Option1" }/>
                         </li>  
 
-                        <Link to ="/apply"> <li className=' hover:bg-gray-300 px-2 py-1 rounded text-center '>Apply </li></Link>
+                        <Link to ="/apply"> <li className=' hover:bg-gray-300 px-2 py-1 rounded md:text-center '>Apply </li></Link>
                         
                         <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group'>
                         <DropBoxLink LinkName={"Interview"} Option_1={"Option1" }/>
