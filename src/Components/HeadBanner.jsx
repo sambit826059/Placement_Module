a39d18429dc97c3cf84f4be7095a4ef33309795f
soumen bhunia <sob99338@gmail.com>
2023-09-24 00:56:02 +0530

    styling change for the headder button [know more ] for the responsiveness

--- src/Components/HeadBanner.jsx
+++ src/Components/HeadBanner.jsx
@@ -7,17 +7,17 @@ export default function HeadBanner(props) {
     <>
     
     
-    <div className="  px-1   ">
+    <div className="  md: pt-4   ">
 
          <div className="relative ">
             <img
-                className="object-cover w-full h-[40vw] sm:h-[20vw] md:h-[15vw] rounded-md"
+                className="object-cover w-full h-[52vw]  md:h-[35vh] rounded-md"
                 src={props.HeadBannerImage}
                 alt="Your Image"
             />
 
-            <button className="bg-gray-200 hover:bg-gray-700 text-gray-800 hover:text-white font-bold py-[0.61vw] px-[0.8vw] rounded absolute top-2 sm:top-4  left-5 mt-2 sm:mt-4 md:mt-6 ml-2 sm:ml-4 md:ml-6  ">
-               <h6 className=' text-[2.5vw] sm:text-[1vw]'>
+            <button className="bg-gray-200 hover:bg-gray-700 text-gray-800 hover:text-white font-bold py-[1.4vh] px-[2vh] rounded absolute top-2 sm:top-4  left-5 mt-2 sm:mt-4 md:mt-6 ml-2 sm:ml-4 md:ml-6  ">
+               <h6 className='  md:text-[2vh]'>
                <a href="/know">Know more</a>
                 </h6> 
             </button>
