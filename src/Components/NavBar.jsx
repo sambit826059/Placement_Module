136bbefe0494bd1b9a4579fe1e52ee8349965d61
soumen bhunia <sob99338@gmail.com>
2023-09-24 12:46:02 +0530

    basic changes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -34,11 +34,11 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                      
                     </div>
                 
-                    <div className={ ` top-[3.5rem]  max-sm:absolute   max-sm:bg-green-200     min-md:relative  min-md:bg-green-00    md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-2 transition-all duration-500 ease-in  ${OpenNavMenu ? 'top-15 max-sm:block ':'top-[-20rem] max-sm:hidden'}   `}>
+                    <div className={ ` top-[3.5rem]  max-sm:absolute   max-sm:bg-green-200    min-md:relative  min-md:bg-green-00    md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-2 transition-all duration-500 ease-in  ${OpenNavMenu ? 'top-15 max-sm:block ':'top-[-20rem] max-sm:hidden'}   `}>
                         
-                        <ul className=' grid  gap-2  sm:flex  sm:gap-4 sm:justify-end '>
+                        <ul className=' grid  gap-2  sm:flex  sm:gap-4 sm:justify-end  '>
 
-                        <Link  to ="/"> <li className='hover:bg-gray-300 px-2 py-1 rounded md:text-center'>Home</li></Link> 
+                        <Link  to ="/"> <li className={`hover:bg-gray-300 px-2 py-1 rounded md:text-center  `}>Home</li></Link> 
 
 
                             <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group'>
