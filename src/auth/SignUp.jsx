90167d57fdde0e9a5860a0d453a793647f9aeef7
soumen bhunia <sob99338@gmail.com>
2023-10-01 20:30:28 +0530

    minor updates [links , defines according to the type of user]

--- src/auth/SignUp.jsx
+++ src/auth/SignUp.jsx
@@ -3,17 +3,14 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Authentication from './Authentication';
 
 export default function SignUp() {
-  document.title = "SignUp [PlacementModule]";
+  document.title = "New User..! [PlacementModule]";
 
-
-  
-
+  const  EntryWayMessage_login="Already have a account";
   return (
-
-
     <>
     <div className="grid place-content-center bg-green-200  h-[100vh]">
-   <Authentication LinkButtonName={'Login'} Links={'/Login'} />
+    <Authentication LinkButtonName={'Login'} Links={'/Login'} SignUpformUse={true} EntryWayMessage={EntryWayMessage_login} />
+
     </div>
     
     </>
