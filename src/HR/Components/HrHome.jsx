1f889abb40a436def01cbcc1d3dc309b883d5a44
soumen bhunia <sob99338@gmail.com>
2023-10-05 00:32:01 +0530

    structure designed

--- src/HR/Components/HrHome.jsx
+++ src/HR/Components/HrHome.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HrHome() {
   return (
     <>
-   <div className='bg-gray-200 p-10 rounded-md'>
+   <div className='bg-blue-200 p-10 rounded-md'>
     this is on right 
    </div>
     </>
