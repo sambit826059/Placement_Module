fa446c8e5a26778d6d979d52837f2865414bded8
soumen bhunia <sob99338@gmail.com>
2023-10-06 01:19:11 +0530

    making this a side bar

--- src/HR/Components/HrNavBar.jsx
+++ src/HR/Components/HrNavBar.jsx
@@ -18,25 +18,21 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
 
   return (
         <>
-        <div className='bg-gray-300 py-10 rounded-md h-[42vw] '>
-
-                    <div className={ `  `}>
-                        
-                        <ul className=' grid  gap-2 p-4     '>
-
-                            <NavLink  to="/HR/home" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded  `}> <li >Dashbord</li></NavLink> 
-
-                            <NavLink to="/HR/jobposting" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded  `}> <li >Job Posting </li></NavLink>
-                            <NavLink to="/HR/Applicants" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded  `}> <li >Applicants </li></NavLink>
-                            
-                            <li className='flex gap-2  px-2 py-1 hover:bg-green-200 rounded group max-sm:text-2xl max-sm:py-3'>
-                                <DropBoxLink LinkName={"Profile"}  Option_2={"Logout" }  />
-                            </li>
-
-                        </ul>
-
-                    </div>
+        <div className='  overflow-hidden'>
         
+            <div class="[sidenav] bg-gray-200 rounded fit-content h-[100vh] grid justify-evenly gap-[2rem]   ">
+            <div class="topToglleMenuButton"></div>
+                <div class="navElements py-1 px-2">
+                    <ul className='grid justify-evenly gap-[1rem]'>
+                        <NavLink  to="/HR/home" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-10 py-1 max-sm:py-3  max-sm:text-2xl rounded `}> <li >Dashbord</li></NavLink> 
+                        <NavLink to="/HR/jobposting" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-10 py-1 max-sm:py-3  max-sm:text-2xl rounded  `}> <li >Job Posting </li></NavLink>
+                        <NavLink to="/HR/Applicants" className={({isActive}) => ` ${ !isActive ? "  hover:bg-gray-300 " : " bg-green-300     hover:bg-green-200  "} hover:bg-green-200  px-10 py-1 max-sm:py-3  max-sm:text-2xl rounded  `}> <li >Applicants </li></NavLink>
+                        <li className='flex gap-2  px-2 py-1 hover:bg-green-200 rounded group max-sm:text-2xl max-sm:py-3 justify-center'>
+                            <DropBoxLink LinkName={"Profile"}  Option_2={"Logout" }  />
+                        </li>           
+                    </ul>
+                </div>
+            </div>
          
         </div>
         </>
