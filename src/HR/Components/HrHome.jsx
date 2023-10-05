07ef11e4e63aa0737b70c2cbd82eb8eead4710b7
soumen bhunia <sob99338@gmail.com>
2023-10-06 01:17:56 +0530

    structure reform

--- src/HR/Components/HrHome.jsx
+++ src/HR/Components/HrHome.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 export default function HrHome() {
   return (
     <>
-   <div className='bg-blue-200 p-10 rounded-md'>
-    this is on right 
+   <div className='overflow-hidden '>
+    <div className=" h-[100vh]   ">
+      <div className="bg-blue-200 h-[100vh]  rounded p-10">
+      this is the Dashbord
+
+      </div>
+
+    </div>
    </div>
     </>
   )
