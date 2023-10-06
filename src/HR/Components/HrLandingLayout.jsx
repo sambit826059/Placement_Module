4d106583a0007f49c3c3a1594611d7f66750fdca
soumen bhunia <sob99338@gmail.com>
2023-10-06 15:16:13 +0530

    whole layout handle in the proper hight

--- src/HR/Components/HrLandingLayout.jsx
+++ src/HR/Components/HrLandingLayout.jsx
@@ -7,7 +7,7 @@ import Footer from '../../Student/Components/Footer'
 export default function HrLandingLayout() {
   return (
     <>
-    <div class="grid grid-cols-[14%,85%] gap-4 overflow-hidden px-4  ">    
+    <div class="grid grid-cols-[14%,85%] gap-2 overflow-hidden px-4  ">    
       <HrNavBar/>
       <Outlet/>
     </div>
