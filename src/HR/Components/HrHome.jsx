de38a72046b0eab09b206fdddffbe2e06bdb3241
soumen bhunia <sob99338@gmail.com>
2023-10-07 20:10:06 +0530

    console error fixeed

--- src/HR/Components/HrHome.jsx
+++ src/HR/Components/HrHome.jsx
@@ -6,7 +6,7 @@ export default function HrHome() {
   
 
    <div className=' overflow-hidden h-[100vh]   rounded-md'>
-        <div class="   fit-content h-[100vh] grid   p-2 ">
+        <div className="   fit-content h-[100vh] grid   p-2 ">
           <div className=" bg-blue-200  rounded-xl p-1 py-10">
           this is the Dashbord
 
