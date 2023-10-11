b5aa0ccd9471d386a747bf696187504b7752432b
soumen bhunia <sob99338@gmail.com>
2023-10-11 18:11:38 +0530

    updating the nav link button

--- src/Student/Components/NavBar.jsx
+++ src/Student/Components/NavBar.jsx
@@ -29,7 +29,7 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                         <input type="checkbox" id='nav-toggle'  className="checkbox-sb group/check hidden"/>
                         <label htmlFor="nav-toggle" className={`label-sb  rounded ${OpenNavMenu ? 'hover:bg-red-200':'hover:bg-gray-300'}  `}>
                             <img onClick={()=>setOpenNavMenu(!OpenNavMenu)} className=' p-2   transition-all duration-00 ease-in cursor-pointer' src={ OpenNavMenu ? [CloseMenu] : [OpenMenu]}   alt="menu-open" />
-8
+
                         </label>
                      
                     </div>
@@ -37,8 +37,8 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                     <div className={ ` top-[3.5rem]  max-sm:fixed   max-sm:bg-green-200    min-md:relative  min-md:bg-green-00    md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-2 max-sm:pl-0 transition-all duration-500 ease-in  ${OpenNavMenu ? 'top-15 max-sm:block ':'top-[-20rem] max-sm:hidden'}   `}>
                         
                         <ul className=' grid  gap-2  sm:flex max-sm:gap-0  sm:gap-4 sm:justify-end  '>
-
-                            <NavLink  to="/Student/Home" className={ ({isActive}) => `  ${ !isActive ? " bg-gray-00  hover:bg-gray-300  after:absolute  after:p-10  after:w-10 before:w-10 " : "bg-gray-00  underline underline-offset-8  hover:bg-gray-300 "}   px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Home</li></NavLink> 
+                        {/* <span class=" invisible group-active:visible before:absolute  before:top-11 before:right-[28rem] before:w-16 before:h-1 before:bg-black"></span> */}
+                            <NavLink  to="/Student/Home" className={ ({isActive}) => `  ${ isActive ? "  before:bg-black " : "  bg-transparent"} hover:bg-gray-300 before:absolute  before:top-11 before:right-[28rem] before:w-16 before:h-1    px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Home </li></NavLink> 
 
 
                             <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3  '>
@@ -46,7 +46,7 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                             <DropBoxLink LinkName={"Resource"}  Option_1={"Check" }/>
                             </li>  
 
-                            <NavLink to="/Student/apply" className={({isActive}) => ` ${isActive ? "bg-gray-00 underline underline-offset-8 hover:bg-gray-300 " : "bg-gray-00   hover:bg-gray-300 "}   px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Apply </li></NavLink>
+                            <NavLink to="/Student/apply" className={({isActive}) => `${ isActive ? "  before:bg-black " : "  bg-transparent"} hover:bg-gray-300 before:rounded-full  before:absolute  before:top-11 before:right-[16rem] before:w-16 before:h-1    px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Apply </li></NavLink>
                             
                             <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3'>
                             <DropBoxLink LinkName={"Interview"} Option_1={"Check Update" }/>
