f3e4d0ee81ccf9ce6a59ddfbe16b663930170cf5
soumen bhunia <sob99338@gmail.com>
2023-10-11 20:51:45 +0530

    hr home updation working on

--- src/HR/Components/HrHome.jsx
+++ src/HR/Components/HrHome.jsx
@@ -6,8 +6,20 @@ export default function HrHome() {
   return (
     <>
   
+  <div className="overflow-hidden rounded-sm border border-stroke bg-white shadow-default dark:border-stroke-dark dark:bg-box-dark">
+        <div className="   fit-content grid   p-2 ">
+          <div className=" bg-blue-200  rounded-xl  ">
+           this is the Dashbord
 
-   <div className=' overflow-hidden h-[100vh]   rounded-md'>
+          </div>
+        </div>
+  </div>
+  
+    </>
+  )
+}
+
+{/* <div className=' overflow-hidden h-[100vh]   rounded-md'>
         <div className="   fit-content h-[100vh] grid   p-2 ">
           <div className=" bg-blue-200  rounded-xl p-1 py-10">
           this is the Dashbord
@@ -15,7 +27,4 @@ export default function HrHome() {
           </div>
         </div>
         
-    </div>
-    </>
-  )
-}
+    </div> */}
