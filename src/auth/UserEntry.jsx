4671418a9b3ed7d5a5e077ce869e47ac3d7fb562
soumen bhunia <sob99338@gmail.com>
2023-10-14 14:47:02 +0530

    landing page fully  responsive

--- src/auth/UserEntry.jsx
+++ src/auth/UserEntry.jsx
@@ -8,18 +8,18 @@ export default function UserEntry() {
 
   return (
     <>
-    <img className='bg-cover bg-center w-[100%]  fixed opacity-110  ' src={landing_pM} alt="bgImg" />
+    <img className='bg-cover bg-center w-screen h-screen  fixed opacity-110   ' src={landing_pM} alt="bgImg" />
 
     <div className="  bg-gradient-to-r fixed flex items-center justify-center w-screen h-screen " >
 
-        <div className=" grid justify-evenly gap-10 opacity-110  p-10 ">
-              <h1 className='text-[7rem] text-white '>Placement Module</h1>
+        <div className="  grid justify-evenly gap-10 opacity-110  p-10 ">
+              <h1 className=' text-[3rem] sm:text-[7rem] text-white text-center '>Placement Module</h1>
 
-              <hr className=' mx-auto my-auto rounded-full w-[34rem]' />
+              <hr className='  border-t-2  mx-auto my-auto w-[75vw] sm:w-[34rem]' />
 
-              <div className=" flex  justify-center  gap-10 py-[2rem] ">
-                    <Link to={'/SignUp'}> <button className=' bg-white  h-[6rem]  w-[18rem] shadow-lg text-[1.4rem]   p-2 px-14 font-light'>Sign Up</button></Link>
-                  <Link to={'/Login'}> <button className=' bg-purple-000 border borde h-[6rem]  w-[18rem] shadow-2xl     text-[1.4rem]   p-2 px-14 font-light text-white'>Login</button></Link>
+              <div className=" flex flex-wrap justify-center  text-center gap-10 py-[2rem] ">
+                    <Link to={'/SignUp'}> <button className=' bg-white hover:opacity-80  sm:h-[6rem]  sm:w-[18rem] shadow-lg text-[1.4rem]   p-2 px-14 font-light    '>Signup</button></Link>
+                  <Link to={'/Login'}> <button className=' hover:bg-purple-500 border-2 hover:opacity-80 sm:h-[6rem]  sm:w-[18rem] shadow-2xl     text-[1.4rem]   p-2 px-14 font-light text-white'>Login</button></Link>
             </div>
         </div>
     </div>
