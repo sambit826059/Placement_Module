26ce145613ee56fc4318de3684ec10f99bc5591a
soumen bhunia <sob99338@gmail.com>
2023-10-26 19:22:15 +0530

    nav bar issue resolve for mobile devices [ navbar invisible on clicking nav links

--- src/Student/Components/NavBar.jsx
+++ src/Student/Components/NavBar.jsx
@@ -36,7 +36,7 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                     <div className={ ` top-[3.5rem]  max-sm:fixed   max-sm:bg-green-200    min-md:relative  min-md:bg-green-00    md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-2 max-sm:pl-0 transition-all duration-500 ease-in  ${OpenNavMenu ? 'top-15 max-sm:block ':'top-[-20rem] max-sm:hidden'}   `}>
                         
                         <ul className=' grid  gap-2  sm:flex max-sm:gap-0  sm:gap-4 sm:justify-end  '>
-                            <NavLink  to="/Student/Home" className={ ({isActive}) => `  ${ isActive ? "  before:bg-slate-700  " : "  bg-transparent"} hover:bg-gray-300 before:absolute  before:top-[3.4rem] before:h-[0.12rem] before:right-[28rem] before:w-16  before:rounded-full    px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Home </li></NavLink> 
+                            <NavLink  to="/Student/Home" onClick={()=>setOpenNavMenu(!OpenNavMenu)} className={ ({isActive}) => `  ${ isActive ? " md:before:bg-slate-700  " : "  bg-transparent"} hover:bg-gray-300 before:absolute  before:top-[3.4rem] before:h-[0.12rem] before:right-[28rem] before:w-16  before:rounded-full    px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Home </li></NavLink> 
 
 
                             <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3  '>
@@ -44,7 +44,7 @@ const [OpenNavMenu,setOpenNavMenu]=useState(false);
                             <DropBoxLink LinkName={"Resource"}  Option_1={"Check" }/>
                             </li>  
 
-                            <NavLink to="/Student/apply" className={({isActive}) => `${ isActive ? "  before:bg-slate-700 " : "  bg-transparent"} hover:bg-gray-300 before:rounded-full  before:absolute  before:top-[3.4rem] before:h-[0.12rem]  before:right-[16.2rem] before:w-[4rem]     px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Apply </li></NavLink>
+                            <NavLink to="/Student/apply" onClick={()=>setOpenNavMenu(!OpenNavMenu)} className={({isActive}) => `${ isActive ? "  md:before:bg-slate-700 " : "  bg-transparent"} hover:bg-gray-300 before:rounded-full  before:absolute  before:top-[3.4rem] before:h-[0.12rem]  before:right-[16.2rem] before:w-[4rem]     px-2 py-1 max-sm:py-3  max-sm:text-2xl rounded md:text-center `}> <li >Apply </li></NavLink>
                             
                             <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3'>
                             <DropBoxLink LinkName={"Interview"} Option_1={"Check Update" }/>
