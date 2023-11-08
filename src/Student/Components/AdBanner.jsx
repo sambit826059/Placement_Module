09697e79564432107e45dbe6c50e5db7518b29f4
soumen bhunia <sob99338@gmail.com>
2023-11-08 23:30:10 +0530

    addign things to this

--- src/Student/Components/AdBanner.jsx
+++ src/Student/Components/AdBanner.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import ADbanner_Card from './ADbanner_Card'
+import Hackathon_bg from "../ImgORicon/bg-hackathon.svg";
+import Internship_bg from "../ImgORicon/bg_internship.svg";
 
 export default function AdBanner() {
   return (
     <>
-    <div className="bg-gray-700  h-[55vh] p-5 rounded ">
-        <div className="flex justify-center  text-white">
-        this banner is for the ad 
+    <div className=" h-[55vh] p-0 gap-3 flex flex-col rounded ">
+      <h1 className='text-2xl'>Explore</h1>
+        <div className='flex gap-[2rem] justify-evenly '> 
+        <ADbanner_Card bg={Internship_bg}/>
+        <ADbanner_Card bg={Hackathon_bg }/>
         </div>
     </div>
     </>
