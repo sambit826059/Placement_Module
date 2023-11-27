277ed4c860295546926120ce780d3834cccc7450
soumen bhunia <sob99338@gmail.com>
2023-11-28 00:52:13 +0530

    dark mode button is connected with localstorage

--- src/HR/Components/HrHeadder.jsx
+++ src/HR/Components/HrHeadder.jsx
@@ -5,10 +5,22 @@ import DropBoxOpen from "../HrImgORicon/NavBarDropDownBtn.svg";
 
 export default function HrHeadder() {
 
-    // const [theaMode, setTheaMode] = useState(false);
-    function ToggleThem(){
-        document.documentElement.classList.toggle("dark");
+   function ToggleThem() {
+    document.documentElement.classList.toggle("dark");
+    const isDarkMode = document.documentElement.classList.contains("dark");
+
+    if (isDarkMode) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.removeItem("theme");
     }
+}
+const storedTheme = localStorage.getItem("theme");
+
+if (storedTheme === "dark") {
+    document.documentElement.classList.add("dark");
+}
+
 
 
   return (
@@ -46,17 +58,19 @@ export default function HrHeadder() {
                       <li>
                       <button>
                                 {/* Dark mode button */}
-                               <label className="    ">
-                                    <input  onClick={ToggleThem}type="checkbox" className="sr-only  peer " />
-                                    <span className="peer peer-checked:bg-green-300  cursor-pointer relative inline-block w-[3.2em] h-[1.8em] transition-all duration-300 ease-in-out bg-gray-300  rounded-full          peer-active:before:w-[]  peer-checked:before:left-[1.6rem]  before:absolute before:content-inside before:top-[0.25rem] before:left-[0.3rem] before:w-[1.3em] before:h-[1.3em] before:bg-white  before:rounded-full  before:transform before:transition-transform before:duration-300 before:ease-in-out  "></span>
-                                </label>                            
+                                <abbr title=" Change Mode">
+                                        <label className="    ">
+                                            <input  onClick={ToggleThem}type="checkbox" className="sr-only  peer " />
+                                            <span className="peer dark:bg-green-300  cursor-pointer relative inline-block w-[3.2em] h-[1.8em] transition-all duration-300 ease-in-out bg-gray-300  rounded-full     before:absolute before:content-inside before:top-[0.25rem] before:left-[0.3rem] dark:before:left-[1.6rem] before:w-[1.3em] before:h-[1.3em] before:bg-white  before:rounded-full  before:transform before:transition-transform before:duration-300 before:ease-in-out  "></span>
+                                        </label>  
+                                </abbr>                          
                         </button>
                      </li>
                         <li className='grid justify-items-stretch gap-0'>
-                            <div className='text-[1rem] justify-self-end dark:text-white'>userNames</div>
-                            <div className='text-gray-500 font-light text-[0.8rem] justify-self-start dark:text-gray-300 '>Company name</div>
+                            <div className='text-[1rem] justify-self-end dark:text-white'>Soumen</div>
+                            <div className='text-gray-500 font-light text-[0.8rem] justify-self-start dark:text-green-300 '>Company name</div>
                         </li>
-                        <li className='flex gap-2 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3'>
+                        <li className='flex gap-2 dark:hover:bg-slate-600 hover:bg-gray-300 px-2 py-1 rounded group max-sm:text-2xl max-sm:py-3'>
                                 <DropBoxLink ProfileImg={'https://picsum.photos/id/200/200/300'} Option_1={"setting" } Option_2={"Logout" }  option2_link={"/login" } />
                             </li>
                     </ul>
