46b3116107df31af3f5116f7fb37c147e2cf1107
Soumen Bhunia <sob99338@gmail.com>
2023-12-03 17:42:23 +0530

    dark them working on

--- src/HR/Components/HrLandingLayout.jsx
+++ src/HR/Components/HrLandingLayout.jsx
@@ -26,12 +26,12 @@ export default function HrLandingLayout() {
       :
       <>
     <div className="">
-      <div className="flex h-screen overflow-hidden">
+      <div className="flex h-screen overflow-hidden  dark:bg-slate-900">
         <HrNavBar/>
         <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
           <HrHeadder/>
           <main>
-            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
+            <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10 ">
               <Outlet />
             </div>
           </main>
